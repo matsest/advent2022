82a155dc9266faf5a1a7c7913ebe0ba776c2ec93
Mats Estensen <mats.est@gmail.com>
2022-12-09 02:05:57 +0100

    day 08 part 2

--- 08/main.go
+++ 08/main.go
@@ -18,11 +18,11 @@ func p1(trees [][]int) int {
 			//fmt.Println("\ncurrent: ", trees[i][j], "(", i, j, ")")
 
 			// Add exterior
-			if i == 0 || i == len(trees) - 1 {
+			if i == 0 || i == len(trees)-1 {
 				visibleTrees = append(visibleTrees, trees[i][j])
 				continue
 			}
-			if j == 0 || j == len(trees[i][:]) - 1 {
+			if j == 0 || j == len(trees[i][:])-1 {
 				visibleTrees = append(visibleTrees, trees[i][j])
 				continue
 			}
@@ -89,6 +89,96 @@ func p1(trees [][]int) int {
 	return len(visibleTrees)
 }
 
+func p2(trees [][]int) int {
+
+	maxScenic := 0
+	for i := 0; i < len(trees); i++ {
+		//fmt.Println("\n row", i)
+		for j:=0; j<len(trees[i]); j++ {
+			// check
+			//fmt.Println(trees[i][j])
+			row := trees[i]
+
+			// count right
+			rightCount := 0
+			latestRight := j
+			for k := j+1; k < len(row)-1; k++ {
+				if row[k] < trees[i][j] {
+					rightCount += 1
+					latestRight = k
+				} else {
+					break
+				}
+			}
+			if j < len(row)-1 && latestRight != len(row)-1 {
+				rightCount += 1
+			}
+
+			// count left
+			leftCount := 0
+			latestLeft := j
+			for k := j-1; k >= 0; k-- {
+				if row[k] < trees[i][j] {
+					leftCount += 1
+					latestLeft = k
+				} else {
+					break
+				}
+			}
+			if j > 0 && latestLeft != 0  {
+				leftCount += 1
+			}
+
+			var col []int
+			for k := range trees {
+				col = append(col, trees[k][j])
+			}
+
+			// count top
+			topCount := 0
+			latestTop := i
+			for k := i-1; k >= 0; k-- {
+				if col[k] < trees[i][j] {
+					topCount += 1
+					latestTop = k
+				} else {
+					break
+				}
+			}
+			if i > 0 && latestTop != 0  {
+				topCount += 1
+			}
+
+			// count bottom
+			bottomCount := 0
+			latestBottom := j
+			for k := i+1; k < len(col)-1; k++ {
+				if col[k] < trees[i][j] {
+					bottomCount += 1
+					latestBottom = k
+				} else {
+					break
+				}
+			}
+			if i < len(col)-1 && latestBottom != len(col)-1 {
+				bottomCount += 1
+			}
+
+			//fmt.Println("right count ", rightCount)
+			//fmt.Println("left count ", leftCount)
+			//fmt.Println("top count ", topCount )
+			//fmt.Println("bottom count ", bottomCount)
+			scenic := rightCount*leftCount*topCount*bottomCount
+			//fmt.Println("scenic ", scenic)
+			if scenic > maxScenic {
+				maxScenic = scenic
+			}
+
+		}
+	}
+    return maxScenic
+}
+
 func main() {
 	lines, _ := utils.ReadLines("input.txt")
 
@@ -101,5 +191,5 @@ func main() {
 	}
 
 	fmt.Println(p1(trees))
-
+	fmt.Println(p2(trees))
 }
