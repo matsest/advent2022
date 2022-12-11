f5d1807bb3757fa2fa8574d73cac4bc1fe0efba4
Mats Estensen <mats.est@gmail.com>
2022-12-11 22:20:31 +0100

    day 10 p2

--- 10/main.go
+++ 10/main.go
@@ -23,7 +23,7 @@ func p1(lines []string) (sum int) {
 
 		if cycle == nextCycleToCheck {
 			fmt.Println("cycle * x: ", cycle*x, cycle, x)
-			sum += cycle*x
+			sum += cycle * x
 			nextCycleToCheck += 40
 		}
 
@@ -37,10 +37,10 @@ func p1(lines []string) (sum int) {
 
 		if remainingWorkingCycles == 0 {
 			if queue[0] != 0 {
-			x += queue[0]
-			//fmt.Println("adding ", queue[0], "to x")
-			queue = queue[1:]
-			remainingWorkingCycles = 1
+				x += queue[0]
+				//fmt.Println("adding ", queue[0], "to x")
+				queue = queue[1:]
+				remainingWorkingCycles = 1
 			} else {
 				queue = queue[1:]
 			}
@@ -55,15 +55,15 @@ func p1(lines []string) (sum int) {
 		//fmt.Println("\ncycle ", cycle, " x is ", x)
 		if cycle == nextCycleToCheck {
 			fmt.Println("cycle * x: ", cycle*x, cycle, x)
-			sum += cycle*x
+			sum += cycle * x
 			nextCycleToCheck += 40
 		}
 
 		if remainingWorkingCycles == 0 {
 			if queue[0] != 0 {
-			x += queue[0]
-			queue = queue[1:]
-			remainingWorkingCycles = 1
+				x += queue[0]
+				queue = queue[1:]
+				remainingWorkingCycles = 1
 			} else {
 				queue = queue[1:]
 			}
@@ -75,9 +75,102 @@ func p1(lines []string) (sum int) {
 	return sum
 }
 
+func p2(lines []string) {
+
+	x, cycle := 1, 1
+	nextCycleToCheck := 40
+	queue := []int{}
+	remainingWorkingCycles := 1
+
+	img := [6][40]string{}
+	row, col := 0, 0
+
+	for _, l := range lines {
+
+		//fmt.Println("\ncycle ", cycle, " x is ", x, "remaining working cycles: ", remainingWorkingCycles)
+		//fmt.Println("queue: ", queue)
+		//fmt.Println(l)
+		parts := strings.Split(l, " ")
+
+		if x-1 <= col && col <= x+1 {
+			img[row][col] = "#"
+		} else {
+			img[row][col] = "."
+		}
+		col += 1
+
+		if cycle == nextCycleToCheck {
+			//fmt.Println("cycle * x: ", cycle*x, cycle, x)
+			row += 1
+			col = 0
+			nextCycleToCheck += 40
+		}
+
+		if parts[0] == "noop" {
+			queue = append(queue, 0)
+		} else if parts[0] == "addx" {
+			num, _ := strconv.Atoi(parts[1])
+			queue = append(queue, num)
+			//fmt.Println("adding to queue: ", num, "queue length ", len(queue))
+		}
+
+		if remainingWorkingCycles == 0 {
+			if queue[0] != 0 {
+				x += queue[0]
+				//fmt.Println("adding ", queue[0], "to x")
+
+				queue = queue[1:]
+				remainingWorkingCycles = 1
+			} else {
+				queue = queue[1:]
+			}
+		} else if remainingWorkingCycles > 0 {
+			remainingWorkingCycles -= 1
+		}
+		cycle += 1
+	}
+
+	// Finish the queue
+	for cycle <= 240 {
+		//fmt.Println("\ncycle ", cycle, " x is ", x)
+
+		if x-1 <= col && col <= x+1 {
+			img[row][col] = "#"
+		} else {
+			img[row][col] = "."
+		}
+		col += 1
+
+		if cycle == nextCycleToCheck {
+			//fmt.Println("cycle * x: ", cycle*x, cycle, x)
+			row += 1
+			col = 0
+			nextCycleToCheck += 40
+		}
+
+		if remainingWorkingCycles == 0 {
+			if queue[0] != 0 {
+				x += queue[0]
+
+				queue = queue[1:]
+				remainingWorkingCycles = 1
+			} else {
+				queue = queue[1:]
+			}
+		} else if remainingWorkingCycles > 0 {
+			remainingWorkingCycles -= 1
+		}
+		cycle += 1
+	}
+	for _, row := range img {
+		fmt.Println(row)
+	}
+}
+
 func main() {
 	lines, _ := utils.ReadLines("input.txt")
 
 	// P1
 	fmt.Println(p1(lines))
+	p2(lines)
 }
