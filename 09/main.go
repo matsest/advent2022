5ab927014a30d31480ebb2320d9b64aef7067c91
Mats Estensen <mats.est@gmail.com>
2022-12-11 14:43:52 +0100

    day 09p1-cleaned

--- 09/main.go
+++ 09/main.go
@@ -47,25 +47,22 @@ func p1(directions []direction) int {
 	area := make(area)
 	area[pos] = coordinate{1, 1}
 
-	// Move
-	for _, v := range directions {
-		pos, tailPos = move(v, pos, tailPos, area)
+	for _, dir := range directions {
+		pos, tailPos = move(dir, pos, tailPos, area)
 	}
 
 	//fmt.Println(area)
-	//fmt.Println(len(area))
-	tot := 0
+	visitedByTail := 0
 	for _, v := range area {
 		if v.visitedByTailCount > 0 {
 			//fmt.Println(c, v)
-			tot += 1
+			visitedByTail += 1
 		}
 	}
-	return tot
+	return visitedByTail
 }
 
 func move(dir direction, pos, tailPos [2]int, a area) ([2]int, [2]int) {
-
 	orientation := dir.orientation
 	length := dir.length
 	//fmt.Println("\n", dir, pos, tailPos)
@@ -75,40 +72,22 @@ func move(dir direction, pos, tailPos [2]int, a area) ([2]int, [2]int) {
 	case "R":
 		for k := pos[0] + 1; k <= pos[0]+length; k++ {
 			currPos = [2]int{k, pos[1]}
-			currPos[0] = k
 			a[currPos] = coordinate{(a[currPos].visitedByHeadCount + 1), (a[currPos].visitedByTailCount)}
 			// Check tail
-			// Move to right
-			if tailPos[1] == currPos[1] && Abs(currPos[0]-tailPos[0]) > 1 {
-				tailPos = [2]int{currPos[0] - 1, tailPos[1]}
-				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving to right", tailPos)
-			}
-			// Move diagonal
-			if tailPos[1] != currPos[1] && Abs(currPos[0]-tailPos[0]) > 1 {
+			if Abs(currPos[0]-tailPos[0]) > 1 {
 				tailPos = [2]int{currPos[0] - 1, currPos[1]}
 				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving diag", tailPos)
 			}
 			//fmt.Println("visiting", currPos, tailPos)
 		}
 	case "L":
 		for k := pos[0] - 1; k >= pos[0]-length; k-- {
 			currPos = [2]int{k, pos[1]}
-			currPos[0] = k
 			a[currPos] = coordinate{(a[currPos].visitedByHeadCount + 1), (a[currPos].visitedByTailCount)}
 			// Check tail
-			// Move to left
-			if tailPos[1] == currPos[1] && Abs(currPos[0]-tailPos[0]) > 1 {
-				tailPos = [2]int{currPos[0] + 1, tailPos[1]}
-				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving to left", tailPos)
-			}
-			// Move diagonal
-			if tailPos[1] != currPos[1] && Abs(currPos[0]-tailPos[0]) > 1 {
+			if Abs(currPos[0]-tailPos[0]) > 1 {
 				tailPos = [2]int{currPos[0] + 1, currPos[1]}
 				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving diag", tailPos)
 			}
 			//fmt.Println("visiting", currPos, tailPos)
 		}
@@ -116,38 +95,22 @@ func move(dir direction, pos, tailPos [2]int, a area) ([2]int, [2]int) {
 	case "U":
 		for k := pos[1] + 1; k <= pos[1]+length; k++ {
 			currPos = [2]int{pos[0], k}
-			a[currPos] = coordinate{(a[currPos].visitedByHeadCount+1), (a[currPos].visitedByTailCount)}
+			a[currPos] = coordinate{(a[currPos].visitedByHeadCount + 1), (a[currPos].visitedByTailCount)}
 			// Check tail
-			// Move up
-			if tailPos[0] == currPos[0] && Abs(currPos[1]-tailPos[1]) > 1 {
-				tailPos = [2]int{currPos[0], tailPos[1] + 1}
-				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving up", tailPos)
-			}
-			// Move diagonal
-			if tailPos[0] != currPos[0] && Abs(currPos[1]-tailPos[1]) > 1 {
+			if Abs(currPos[1]-tailPos[1]) > 1 {
 				tailPos = [2]int{currPos[0], currPos[1] - 1}
 				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving diag", tailPos)
 			}
 			//fmt.Println("visiting", currPos, tailPos)
 		}
 	case "D":
 		for k := pos[1] - 1; k >= pos[1]-length; k-- {
 			currPos = [2]int{pos[0], k}
-			a[currPos] = coordinate{(a[currPos].visitedByHeadCount+1), (a[currPos].visitedByTailCount)}
+			a[currPos] = coordinate{(a[currPos].visitedByHeadCount + 1), (a[currPos].visitedByTailCount)}
 			// Check tail
-			// Move up
-			if tailPos[0] == currPos[0] && Abs(currPos[1]-tailPos[1]) > 1 {
-				tailPos = [2]int{tailPos[0], tailPos[1] - 1}
-				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving down", tailPos)
-			}
-			// Move diagonal
-			if tailPos[0] != currPos[0] && Abs(currPos[1]-tailPos[1]) > 1 {
+			if Abs(currPos[1]-tailPos[1]) > 1 {
 				tailPos = [2]int{currPos[0], currPos[1] + 1}
 				a[tailPos] = coordinate{(a[tailPos].visitedByHeadCount), (a[tailPos].visitedByTailCount + 1)}
-				//fmt.Println("tail moving diag", tailPos)
 			}
 			//fmt.Println("visiting", currPos, tailPos)
 		}
@@ -156,12 +119,9 @@ func move(dir direction, pos, tailPos [2]int, a area) ([2]int, [2]int) {
 }
 
 func main() {
-	lines, _ := utils.ReadLines("input.txt")
-
-	// Part 1
+	lines, _ := utils.ReadLines("test.txt")
 	directions := parseInput(lines)
 	//fmt.Println("directions: ", directions)
 
 	fmt.Println(p1(directions))
-
 }
